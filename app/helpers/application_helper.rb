ea672b069b802d547b13ffd8f285fb225128ddd3
suzuki_takuro <takuro.suzuki@drecom.co.jp>
2013-08-10 21:18:09 +0900

    マークダウン

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown(text) 
+  	options = [:hard_wrap, :filter_html, :autolink,  
+      :no_intraemphasis, :fenced_code, :gh_blockcode] 
+   # Markdown.new(text, extensions = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode])
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :filter_html => true, :autolink => true, :space_after_headers => true, :fenced_code => true, :hard_wrap => true)
+	return markdown.render(text).html_safe
+  end
 end
